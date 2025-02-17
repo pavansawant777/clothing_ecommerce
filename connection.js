20c0a3dc36799040ef2a6a4582503763f0eed59b
pavansawant777 <126850815+pavansawant777@users.noreply.github.com>
2025-02-17 22:36:05 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
  var mysql= require("mysql");
  var util = require("util");
  var conn = mysql.createConnection({
-    "host":"localhost",
-    "user":"root",
-    "password":"",
-    "database":"fashion_node"
+    "host":"bio7kd7hjw3g3yvkyxbb-mysql.services.clever-cloud.com",
+    "user":"usdkc0dc1jbwdogz",
+    "password":"lDZPPDNrDPgiSHLnnOcV",
+    "database":"bio7kd7hjw3g3yvkyxbb"
     
  });
 
